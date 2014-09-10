12f2f96e8753d61171d7231f18b94ba26ddf13dc
Sean McMurray <scmcmurray@gmail.com>
2014-09-09 18:36:44 -0700

    Module wrapper & minimized parser

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-(function(){
+(function(definition){
   'use strict';
 
-  var compiler = require('./parser');
-  
-  module.exports = Object.create(Object.prototype, {
+  if (typeof module != 'undefined') {
+    module.exports = definition(require);
+  }
+  else if (typeof define == 'function' && typeof define.amd == 'object') {
+    define(definition);
+  }
+  else throw new Error('Kata requires a module loader. Please use a CommonJS or AMD module loader.');
+}(function(require) {
+
+  var compiler = require('./parser.min');
+
+  return Object.create(Object.prototype, {
     compile: {
       value: function(src, options){
-        return compiler.parse(src);
+        var defaults = {
+          browser: true
+        };
+        var opts = Object.keys(defaults).reduce(function(o,k){
+          if (options && options[k]) o[k] = options[k];
+          else o[k] = defaults[k];
+          return o;
+        }, {});
+
+        var res;
+        try {
+          res = compiler.parse(src);
+        }
+        catch(ex){
+          throw new Error(compiler.SyntaxError);
+        }
+        return "(function(name, definition){"
+          + "'use strict';"
+          + "if (typeof module != 'undefined') module.exports = definition();"
+          + "else if (typeof define == 'function' && typeof define.amd == 'object') define(definition);"
+          + "else this[name] = definition();"
+          + "}('root'," + res + "));"
       }
     }
   });
-}());
+}));
