842d11cccf4005027ef21b84f392b72a6e7bea76
Sean McMurray <sean@mcmurrays.org>
2014-09-15 06:19:53 -0700

    Finished making blk[] definitions inherit Block. changed core while loop to use Blocks. cleaned up various syntax.

--- parser.js
+++ parser.js
@@ -6,13 +6,15 @@
     define(definition());
   }
   else this[name] = definition();
-}('kata', function(str, options){
+}('kata', function(){
   var blk = {};
 
   var Block = Object.create(Object.prototype, {
     parse: {
       value: function(str){
-        this.addChild('c').parse(str);
+        if (str && /\S/.test(str))
+          this.addChild('c').parse(str);
+        this.parsed = true;
       }
     }
     , end: {value: null, writable: true}
@@ -22,7 +24,18 @@
         var n = this.children.push(Object.create(blk[symbol||'='], {
           start: {value: start}
         }));
+        console.log('Added child ', symbol);
         return this.children[n-1];
+      }
+    }
+  });
+
+  blk['root'] = Object.create(Block, {
+    parsed: { value: true }
+    , addChild: {
+      value: function(sym, start){
+        if ('%' != sym) throw new Error('Expected Template block. Templates must begin with {{%');
+        Block.addChild.call(this, sym, start);
       }
     }
   });
@@ -44,8 +57,7 @@
         this.args = match.value;
         this.end += match.pos;
 
-        if (match.pos < str.length)
-          Block.parse.call(this, str.substr(match.pos));
+        Block.parse.call(this, str.substr(match.pos));
         return this;
       }
     }
@@ -55,7 +67,7 @@
           + "var out='';"
           + this.children.reduce(function(o,b){
             return o+b.render();
-          }, '');
+          }, '')
           + "}";
       }
     }
@@ -70,38 +82,41 @@
   blk['='] = Object.create(Block, {
     parse: {
       value: function(str){
-        this.expr = str;
+        this.expr = str.replace(/"/g, '\\"').replace(/\r|\n/g, '\\n');
+        Block.parse.call(this);
         return this;
       }
     }
     , render: {
       value: function(){
-      return 'out += ' + this.expr.replace(/"/g, '\\"').replace(/\r|\n/g, '\\n') + ';';
+        return 'out += ' + this.expr + ';';
+      }
     }
     , addChild: {
       value: function(){
         throw new Error('Interpolate blocks cannot contain other blocks');
       }
     }
-  };
+  });
 
   blk['@'] = Object.create(Block, {
-    parse: {sig: function(str){
-      var match, pos;
-      if (!(match = parenthetical(str))) throw new Error('Iterate block missing iterable');
-      this.iter = match.value;
-      str = str.substr(match.pos);
-      if (!(match = parenthetical(str))) throw new Error('Iterate block missing arguments');
-      this.args = match.value;
-      if (match.pos < str.length)
+    parse: {
+      value: function(str){
+        var match;
+        if (!(match = parenthetical(str))) throw new Error('Iterate block missing iterable');
+        this.iter = match.value;
+        str = str.substr(match.pos);
+        if (!(match = parenthetical(str))) throw new Error('Iterate block missing arguments');
+        this.args = match.value;
         Block.parse.call(this, str.substr(match.pos));
-      return this;
-    },
-    render: function(){
+        return this;
+      }
+    }
+    , render: function(){
       return this.iter+'.forEach(function'+this.args+'{'
         + this.children.reduce(function(o,b){
           return o+b.render();
-        }, '');
+        }, '')
         + '});';
     }
     , addChild: {
@@ -110,18 +125,17 @@
         return Block.addChild.call(this, sym, start);
       }
     }
-  };
+  });
 
   blk['?'] = Object.create(Block, {
     parse: {
       value: function(str){
-      var match;
-      if (!(match = parenthetical(str))) throw new Error('Condition block missing condition');
-      this.cond = match.value;
-      if (match.pos < str.length){
+        var match;
+        if (!(match = parenthetical(str))) throw new Error('Condition block missing condition');
+        this.cond = match.value;
         Block.parse.call(this, str.substr(match.pos));
+        return this;
       }
-      return this;
     }
     , render: function(){
       return 'if '+this.cond+'{'
@@ -130,7 +144,7 @@
         }, '')
         + '}';
     }
-  };
+  });
 
   blk[':'] = Object.create(Block, {
     parse: {
@@ -140,9 +154,7 @@
           this.cond = match.value;
           str = str.substr(match.pos);
         }
-        if (match.pos < str.length){
-          Block.parse.call(this, str.substr(match.pos))
-        }
+        Block.parse.call(this, str.substr(match.pos))
         return this;
       }
     }
@@ -154,94 +166,127 @@
         }, '');
       }
     }
-  };
+  });
 
   blk['+'] = Object.create(Block, {
     parse: {
       value: function(str){
-      var match, re=/^\s*(\w+)/g;
-      if (!(match = re.exec(str))) throw new Error('Import block missing alias');
-      this.alias = match.value;
-      str = str.substr(re.lastIndex);
-      if (!(match=parenthetical(str))) throw new Error('Import block missing template location');
-      this.location = match.value;
-      str = str.substr(match.pos);
-      if (match.pos <str.length){
+        var match, re=/^\s*(\w+)/g;
+        if (!(match = re.exec(str))) throw new Error('Import block missing alias');
+        this.alias = match.value;
+        str = str.substr(re.lastIndex);
+        if (!(match=parenthetical(str))) throw new Error('Import block missing template location');
+        this.location = match.value;
+        str = str.substr(match.pos);
         Block.parse.call(this, str);
-      }
 
-      return this;
+        return this;
+      }
     }
     , render: {
-        value: function(){
+      value: function(){
         return 'var ' + this.alias + ' = require' + this.location + ';';
+      }
     }
     , addChild: {
-      value: function(){ return throw new Error('Import blocks cannot contain other blocks.')}
+      value: function(){ throw new Error('Import blocks cannot contain other blocks.')}
     }
-  };
+  });
 
-  blk['>'] = {sig: function(str){
-      var match, re=/^\s*(\w+)/g;
-      var sig = {target: null
-        , args: null
-        , pos: 0
-      };
-      if (!(match=re.exec(str))) throw new Error('Invoke block missing template name');
-      sig.target = match[1];
-      sig.pos += re.lastIndex;
-      str = str.substr(re.lastIndex);
+  blk['>'] = Object.create(Block, {
+    parse: {
+      value: function(str){
+        var match, re=/^\s*(\w+)/g;
+        if (!(match=re.exec(str))) throw new Error('Invoke block missing template name');
+        this.signature = match[1];
+        this.end += re.lastIndex;
+        str = str.substr(re.lastIndex);
 
-      if (!(match = parenthetical(str))) throw new Error('Invoke block missing ()');
-      sig.args = match.value;
-      sig.pos += match.pos;
-      return sig;
+        if (!(match = parenthetical(str))) throw new Error('Invoke block missing ()');
+        this.signature += match.value;
+        this.end += match.pos;
+        Block.parse.call(this, str.substr(match.pos));
+
+        return this;
+      }
     }
-    , render: function(sig, blocks){
-      return 'out+=(function(){'
-        + blocks.reduce(function(o,b){
-            if ('%' !== b.sym) throw new Error('Invoke blocks can only contain Template blocks,');
-            return o+b.value;
+    , render: {
+      value: function(){
+        return 'out+=(function(){'
+          + this.children.reduce(function(o,b){
+              return o+b.render();
+            }, '')
+          + this.signature + ';}());';
+      }
+    }
+    , addChild: {
+      value: function(sym, start){
+        if ('%' != sym) throw new Error('Invoke blocks can only contain Template blocks,');
+        Block.addChild.call(this, sym, start);
+      }
+    }
+  });
+
+  blk['<'] = Object.create(Block, {
+    parse: {
+      value: function(str){
+        var match, re=/^\s*(\w+)/g;
+        if (!(match=re.exec(str))) throw new Error('Yield block missing template name');
+        this.target = match[1];
+        str = str.substr(re.lastIndex);
+
+        if (!(match = parenthetical(str))) throw new Error('Yield block missing ()');
+        this.args = match.value;
+        str = str.substr(match.pos);
+        Block.parse.call(this, str);
+        return this;
+      }
+    }
+    , render: {
+      value: function(){
+        return 'if (typeof '+target+' ==="function") out+='+this.target + this.args + '; else {'
+        + this.childdren.reduce(function(o,b){
+            return o+b.render();
           }, '')
-        + sig.target + sig.args + ';}());';
+        + '}';
+      }
     }
-  };
+  });
 
-  blk['<'] = {sig: function(str){
-      var match, target, args, re=/^\s*(\w+)/g;
-      if (!(match=re.exec(str))) throw new Error('Yield block missing template name');
-      target = match[1];
-      str = str.substr(re.lastIndex);
+  blk['!'] = Object.create(Block, {
+    parse: {
+      value: function(str){
+        this.expr = str;
+        Block.parse.call(this);
+        return this;
+      }
+    }
+    , render: {
+      value: function(){
+        return this.expr;
+      }
+    }
+    , addChild: {
+      value: function(){
+        throw new Error('Evaluate blocks cannot contain other blocks');
+      }
+    }
+  });
 
-      if (!(match = parenthetical(str))) throw new Error('Yield block missing ()');
-      args = match.value;
-      str = str.substr(match.pos);
+  blk['c'] = Object.create(Block, {
+    parse: {
+      value: function(str){
+        this.expr = str.replace(/"/g, '\\"').replace(/\r|\n/g, '\\n');
+        Block.parse.call(this);
+        return this;
+      }
     }
-    , render: function(sig, blocks){
-      return 'if (typeof '+target+' ==="function") out+='+sig.target + sig.args + '; else {'
-      + blocks.reduce(function(o,b){
-          return o+b.value;
-        }, '')
-      + '}';
+    , render: {
+      value: function(){
+        return 'out+="' + this.expr + '";';
+      }
     }
-  };
-
-  blk['!'] = {sig: function(str){
-      return { expr: str , pos: str.length };
-    }
-    , render: function(sig, blocks){
-      if (blocks.length) throw new Error('Evaluate blocks cannot contain other blocks');
-      return sig.expr;
-    }
-  };
-
-  blk['c'] = {sig: function(str){
-      return {content: str};
-    }
-    , render: function(sig, blocks){
-      return 'out+="' + sig.content.replace(/"/g, '\\"').replace(/\r|\n/g, '\\n') + '";';
-    }
-  };
+  });
 
   function parenthetical(str){
     var match, re = /^\s*\(/g;
@@ -263,8 +308,8 @@
     throw new Error('Parenthetical missing )');
   }
 
-  return function(str){
-    var root = {sym: 'root', s: 0, blocks: []}, cur=root;
+  return function(str, options){
+    var root = Object.create(blk['root'], {start: {value: 0}}), cur=root;
     var q = [root], tmp;
     var match, re=/((?:(?!\{\{[%@\?:\+><!]?|[%@\?:\+><!]?\}\})[\s\S])*)(\{\{[%@\?:\+><!]?|[%@\?:\+><!]?\}\})/g;
 
@@ -272,56 +317,26 @@
       //Everything between the last match and now is content
       //Or could be block signature + content
       if (match[1] && match[1].length){
-        if (cur.sig){ // This block already has a sig. So match[1] must be content.
-          cur.blocks.push({sym: 'c'
-            , s: match.index
-            , e: re.lastIndex-match[2].length
-            , sig: blk['c'].sig(match[1])
-            , blocks:[]
-          });
+        console.error('Cur: ', cur);
+        if (cur.parsed){ // This block has already been parsed. So match[1] must be content.
+          cur.addChild('c', match.index).parse(match[1]);
         }
         else {
-          try {
-            cur.sig = blk[cur.sym].sig(match[1]);
-          }
-          catch(ex){
-            throw new Error(ex + ': block starting at position ' + cur.s);
-          }
-          if (cur.sig.pos < match[1].length){ // There is content after the sig.
-            cur.blocks.push({sym: 'c'
-              , s: cur.sig.pos
-              , e: re.lastIndex-match[2].length
-              , sig: blk['c'].sig(match[1].substr(cur.sig.pos))
-              , blocks:[]
-            });
-          }
+          cur.parse(match[1]);
         }
       }
       if ('{{' === match[2].substr(0,2)){
-        sym = match[2].substr(2) || '=';
-
-        tmp = cur.blocks.push({sym: sym, s: re.lastIndex-match[2].length, blocks: []});
-        q.push(cur.blocks[tmp-1]);
+        q.push(cur.addChild(match[2].substr(2) || '=', re.lastIndex-match[2].length));
       }
-      else if (cur.sym === 'root') {
+      else if (cur.symbol === 'root') {
         throw new Error('Expecting {{% but found ' + match[2] + ' at position ' + (re.lastIndex-match[2].length));
       }
       else {
         /* This must be a }}
-        If so, all the child blocks have been rendered.
-        We are now ready to render this block.*/
-
-        /* First make sure start and end symbols match */
-        sym = (3 === match[2].length ? match[2].substr(0,1) : cur.sym);
-        if (sym && sym != cur.sym) throw new Error('{{'+cur.sym+' at position ' + cur.s + ' does not match ' + sym +'}} at position '+re.lastIndex-3);
+        /* Make sure start and end symbols match */
+        sym = (3 === match[2].length ? match[2].substr(0,1) : this.symbol);
+        if (sym && sym != cur.symbol) throw new Error('{{'+cur.symbol+' at position ' + cur.s + ' does not match ' + sym +'}} at position '+re.lastIndex-3);
         cur.e = re.lastIndex;
-        try {
-          cur = {sym: cur.sym, value: blk[sym].render(cur.sig, cur.blocks)};
-          console.log('*** ' + cur.value + ' ***');
-        }
-        catch(ex){
-          throw new Error(ex + ' in block from position ' + cur.s + ' to position ' + cur.e);
-        }
         q.pop();
       }
       cur = q[q.length-1];
@@ -336,7 +351,5 @@
     if (tmp && /\S/.test(tmp)) throw new Error('Unexpected content after template at position ' + root.e);
 
     return root.blocks.reduce(function(o,b){return o+b.value}, '');
-
-
   }
 }));
