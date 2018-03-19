30f3611f47d38a5759e0a4a7e891bbb704db5f39
Sean <scmcmurray@gmail.com>
2018-03-19 16:33:23 -0700

    Vanity of a smaller dist

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let re = / on([a-z]+)\s*=\s*(["'])?\s*?$/
-let quote = {"'": /^\s*'/, '"': /^\s*"/}
+  ,quote = {"'": /^\s*'/, '"': /^\s*"/}
+  ,ehs = 'data-kata-ev'
+  ,ecs = 'data-kata-embed'
 
 export default function(s, ...expr){
-  let strings = Array.from(s)
-  let predoc = ''
-  let handlers = []
-  let embeds = []
+  let e,embeds = []
+      ,handlers = []
+      ,predoc = ''
+      ,strings = Array.from(s)
   for (let e of expr) {
     if ('function'===typeof e){
       predoc += strings.shift().replace(re, (_, event, q)=>{
@@ -13,43 +15,36 @@ export default function(s, ...expr){
         if (q && strings[0]){
           strings[0] = strings[0].replace(quote[q], '')
         }
-        return ` data-event-handler-replacement="${i}"`
+        return ` ${ehs}="${i}"`
       })
     }
     else {
       predoc += strings.shift()
-      for (let ae of Array.isArray(e) ? e : [e]){
-        if ('string' === typeof ae){
-          predoc += ae
+      for (let ae of [].concat(e)){
+        if (('object' === typeof ae) && ('number' === typeof ae.nodeType) && [
+          1 // Element
+          ,3 // Text
+          ,8 // Comment
+          ,11 // DocumentFragment
+        ].includes(ae.nodeType)){
+          let i = embeds.push(ae)-1
+          predoc += `<div ${ecs}="${i}"></div>`
         }
-        else if (('object' === typeof ae) && ('number' === typeof ae.nodeType)){
-          switch(ae.nodeType){
-            case 1: // Element
-            case 3: // Text
-            case 8: // Comment
-            case 11: // DocumentFragment
-              let i = embeds.push(ae)-1
-              predoc += `<div data-embedded-content-replacement="${i}"></div>`
-          }
-        }
-        else {
-          console.log('unknown chunk', ae)
-        }
+        else predoc += ae
       }
     }
   }
   predoc+=strings.shift()
   let doc = document.createRange().createContextualFragment(predoc.trim())
-  doc.querySelectorAll('*[data-event-handler-replacement]').forEach(e=>{
-    let h = handlers[e.getAttribute('data-event-handler-replacement')]
+  doc.querySelectorAll(`*[${ehs}]`).forEach(e=>{
+    let h = handlers[e.getAttribute(ehs)]
     e.addEventListener(h.event, h.handler)
-    e.removeAttribute('data-event-handler-replacement')
+    e.removeAttribute(ehs)
   })
-  doc.querySelectorAll('*[data-embedded-content-replacement]').forEach(e=>{
-    let c = embeds[e.getAttribute('data-embedded-content-replacement')]
-    e.parentNode.insertBefore(c, e)
-    e.parentNode.removeChild(e)
+  doc.querySelectorAll(`*[${ecs}]`).forEach(e=>{
+    let c = embeds[e.getAttribute(ecs)]
+    e.before(c, e)
+    e.remove()
   })
-  if (1==doc.childNodes.length) doc = doc.firstChild
-  return doc
+  return 1==doc.childNodes.length ? doc.firstChild : doc
 }
